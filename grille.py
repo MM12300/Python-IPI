611f582f60b20915be50c269ebc05824fd80f9c9
MM12300 <nerggalkore@gmail.com>
2020-10-02 22:13:37 +0200

    on dirait que ça va marcher

--- grille.py
+++ grille.py
@@ -1,18 +1,40 @@
-import matplotlib
-import matplotlib.pyplot as plt
 import numpy as np
 
-# Data for plotting
-t = np.arange()
-# s = 1 + np.sin(2 * np.pi * t)
+grille=[]
 
-fig, ax = plt.subplots()
-ax.plot(t)
+def brokenGrid(m,n):
+    a= [0 for i in range(m)]
+    print(a)
+    for n in range(n):
+        grille.append(a)
+    print(grille)
 
-ax.set(xlabel='X', ylabel='Y',
-       title='plot')
-ax.grid()
+def vanillaGrid(m,n):
+    for n in range(n):
+        grille.append(['0' for i in range(m)])
+    print(grille)
 
-fig.savefig("test.png")
-plt.show()
+def npGrid(m,n):
+    grille = np.array([[0] * m] * n)
+    return grille
 
+def main():
+       vanillaGrid(5,5)
+       grille[1][1] = "ÇA MARCHE"
+       print(grille)
+
+if __name__=='__main__':
+    main()
+
+total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] + 
+                         grid[(i-1)%N, j] + grid[(i+1)%N, j] + 
+                         grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] + 
+                         grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/255) 
+  
+            # apply Conway's rules 
+            if grid[i, j]  == ON: 
+                if (total < 2) or (total > 3): 
+                    newGrid[i, j] = OFF 
+            else: 
+                if total == 3: 
+                    newGrid[i, j] = ON 
--- jeudelavie.py
+++ jeudelavie.py
@@ -1,31 +1,63 @@
-def gridDesign(m,n):
-    a = [0 for i in range(m)]
-    b = [a for x in range(n)]
-    for key in b:
+import numpy as np
+import random
+
+grille=[]
+
+# def brokenGrid(m,n):
+#     a= [0 for i in range(m)]
+#     print(a)
+#     for n in range(n):
+#         grille.append(a)
+#     print(grille)
+
+def vanillaGrid(m,n):
+    for n in range(n):
+        grille.append([random.randrange(0,2) for i in range(m)])
+    print(grille)
+
+# def npGrid(m,n):
+#     grille = np.array([[0] * m] * n)
+#     return grille
+
+def transfoGrid(grid):
+    for key in grid :
         print(key)
 
+def cellState(x,y):
 
-def main(): 
-    gridDesign(5,5)
+    neighborhood=int(grille[x-1][y-1]+grille[x-1][y]+grille[x-1][y+1]
+                    +grille[x][y-1]+grille[x][y+1]
+                    +grille[x+1][y-1]+grille[x+1][y]+grille[x+1][y+1])
+
+    print (neighborhood)
+
+    if (neighborhood < 2) or (neighborhood > 3):
+        grille[x][y]=0
+    elif neighborhood == 3:
+        grille[x][y]=1
+    
+    return print(transfoGrid(grille))
+
+def gridCellState(grille):
+    #à appliquer la fonction cellstate à toutes les entrées du tableau
+    for grille in grille:
+        for i in grille:
+            print (i)
+
+    
+
+
+def main():
+       vanillaGrid(3,3)
+       print(transfoGrid(grille))
+       cellState(1,1)
+       gridCellState(grille)
+
+
+
+
+
+
 
 if __name__=='__main__':
     main()
-
-
-# def makeArray(x):
-#     a = [0 for i in range(x)]
-#     # print(array)
-
-# def makeGrid(y):
-#     a=makeArray(6)
-#     return [a for j in range(y)]
-
-# def afficheGrid(a):
-#     for key in a:
-#         print(key)
-
-# def main():
-#     grid=makeGrid(6)
-#     afficheGrid(grid)
-
-
