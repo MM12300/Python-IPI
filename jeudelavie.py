8999158f2e60fc3e5dd6b2b01a2a653fb5a697b8
MM12300 <nerggalkore@gmail.com>
2020-10-04 16:29:25 +0200

    retour case départ

--- jeudelavie.py
+++ jeudelavie.py
@@ -8,13 +8,41 @@ import random
 #         grille.append(a)
 #     print(grille)
 
+#-----------------------------------
 # def npGrid(m,n):
 #     grille = np.array([[0] * m] * n)
 #     return grille
 
 
-grille=[]
+#------------------------------------------
+#Si on fait cell State de [0][0] on trouve bien 8, preuve que on est dans le modèle torus
+# grilleTorus=[
+#     [0,1,0,0,1],
+#     [1,1,0,0,1],
+#     [0,0,0,0,0],
+#     [0,0,0,0,0],
+#     [1,1,0,0,1]
+# ]
 
+#---------------------------------------------
+# printcontenu de chaque cellule
+# for list in grille:
+#         for cell in list:
+#             print(cell)
+
+
+
+#----------------------------------------------------
+# for ligne in grille:
+#             x = len(ligne)
+#             for cell in range(x):
+#                 z = (grille.index(ligne),cell)
+#                 print(z)
+
+
+
+
+grille=[]
 
 
 def vanillaGrid(m,n):
@@ -39,9 +67,18 @@ def cellState(y,x):
         )
 
 
-
     #si x=0 : retirer les x-1
+    if x == 0 : 
+        neighborhood=int(
+        +grille[x][y-1]
+        +grille[x][y+1]
+        +grille[x+1][y-1]
+        +grille[x+1][y]
+        +grille[x+1][y+1]
+        )
+    
     #si x=longueur tableau, retirer les x+1
+
     #si y=0 : retirer les y-1
     #si y=longueur tableau, retirer les y+1
     #si x=0 et y=0, retirer les x-1 et y-1
@@ -50,7 +87,6 @@ def cellState(y,x):
     #si x=0 et y=longueur, retirer x-1 et y+1
 
     
-    neighborhood=3
     print(neighborhood)
     if (neighborhood < 2) or (neighborhood > 3):
         grille[y][x]=0
@@ -62,24 +98,68 @@ def cellState(y,x):
    
 
 def gridSize(grille,m,n):
-    vanillaGrid(m,n)
-    for list in grille:
-        for cell in list:
-            print(cell)
-    for m in grille:
-        print(grille.index(m))
-        for n in grille:
-            print(grille.index(n))
-            cellState(grille.index(m), grille.index(n))
+    # vanillaGrid(m,n)
 
- 
+    grille =[
+    [0,1,0,0,1],
+    [1,1,0,0,1],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [1,1,0,0,1]
+]
+    
+    # for list in grille:
+    #     for cell in list:
+    #         print(cell)
+    # for m in grille:
+    #     print(grille.index(m))
+    #     for n in grille:
+    #         print(grille.index(n))
+    #         cellState(grille.index(m), grille.index(n))
+
+
+    # for ligne in grille:
+    #     #permet de connaître Y
+    #     # print(grille.index(list))
+    #     for cell in ligne:
+
+    # cellState(
+    #     grille.index
+    # )
+
+
+    # print(transfoGrid(grille))
+
+
+
+
+
+
+def gameOfLife(grille,m,n):
+    #Créer une grille : 
+    grille=[]
+
+
+
+
 
 
 def main():
     vanillaGrid(5,5)
     print(transfoGrid(grille))
-    cellState(3,1)
-    print(transfoGrid(grille))
+    for ligne in grille:
+            x = len(ligne)
+            print(x)
+            for cell in range((x)):
+                z = (grille.index(ligne),cell)
+                print(z)
+                cellState(z[0],z[1])
+    cellState(4,0)
+            
+
+
+
+
     
 
 
@@ -92,3 +172,11 @@ def main():
 
 if __name__=='__main__':
     main()
+
+
+# 1 - Créer une grille rempli de nombre aléatoire entre 0 et 1, 0 = mort et 1 = vivant
+# 2 - Créer une fonction qui permet de changer la valeur d'une cellule en fonction des règles du jeu de la vie 
+# ATTENTION, cette fonction marche dans le modèle TORON/DONUT
+# 3 - Appliquer cette règle à chacun des cellules du tableau
+# 4 - Répéter autant de fois que faire se peut : BOUCLE
+
