4291b154b77ab2e2598299c98797f7eff0c56e23
MM12300 <nerggalkore@gmail.com>
2020-10-04 19:58:43 +0200

    ça marche !

--- dimanche.py
+++ dimanche.py
@@ -1,5 +1,7 @@
 import random
 import numpy as np
+import os
+import time
 
 grille=[]
 
@@ -29,37 +31,44 @@ def cap (a, arrayL):
 
 
 def main():
-    vanillaGrid(5,5)
-    print(transfoGrid(grille))
-    for ligne in grille:
-        m = len(ligne)
-        grilleL= len(grille)-1
-        ligneL = len(ligne)-1
+    #Création grille
+    vanillaGrid(10,10)
 
-        for cell in range(m):
-            y= grille.index(ligne)
-            x= cell
-            # z = (y,x)    
-  
-            neighborhood=int(
-                                grille[cap(x-1,ligneL)][cap(y-1, grilleL)]
-                                +grille[cap(x-1,ligneL)][cap(y,grilleL)]
-                                +grille[cap(x-1,ligneL)][cap(y+1,grilleL)]
-                                +grille[cap(x,ligneL)][cap(y-1, grilleL)]
-                                +grille[cap(x,ligneL)][cap(y+1,grilleL)]
-                                +grille[cap(x+1,ligneL)][cap(y-1, grilleL)]
-                                +grille[cap(x+1,ligneL)][cap(y,grilleL)]
-                                +grille[cap(x+1,ligneL)][cap(y+1,grilleL)]
-                                )
-            
-            if (neighborhood < 2) or (neighborhood > 3):
-                #dead
-                grille[y][x]=0
-            elif neighborhood == 3:
-                #alive
-                grille[y][x]=1
-    print(transfoGrid(grille))
+    #Itération
+    for n in range(30):
+
+        for ligne in grille:
+            m = len(ligne)
+            grilleL= len(grille)-1
+            ligneL = len(ligne)-1
+
+            for cell in range(m):
+                y= grille.index(ligne)
+                x= cell
+                # z = (y,x)    
+    
+                neighborhood=int(
+                                    grille[cap(x-1,ligneL)][cap(y-1, grilleL)]
+                                    +grille[cap(x-1,ligneL)][cap(y,grilleL)]
+                                    +grille[cap(x-1,ligneL)][cap(y+1,grilleL)]
+                                    +grille[cap(x,ligneL)][cap(y-1, grilleL)]
+                                    +grille[cap(x,ligneL)][cap(y+1,grilleL)]
+                                    +grille[cap(x+1,ligneL)][cap(y-1, grilleL)]
+                                    +grille[cap(x+1,ligneL)][cap(y,grilleL)]
+                                    +grille[cap(x+1,ligneL)][cap(y+1,grilleL)]
+                                    )
+                
+                if (neighborhood < 2) or (neighborhood > 3):
+                    #dead
+                    grille[y][x]=0
+                elif neighborhood == 3:
+                    #alive
+                    grille[y][x]=1
+        transfoGrid(grille)
+        time.sleep(1)
+        os.system('cls||clear')
+        
 
 if __name__=='__main__':
-    vanillaGrid(5,5)
-    transfoGrid(grille)
+    main()
+    
